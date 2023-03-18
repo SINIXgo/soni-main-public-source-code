9832a99e6e56e814218820284c4e643fa00b3f62
SINIX_SKR <116418436+SINIXgo@users.noreply.github.com>
2023-03-18 13:02:01 -0300

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -444,7 +444,7 @@ class ChartingState extends MusicBeatState
 			var songName:String = Paths.formatToSongPath(_song.song);
 			var file:String = Paths.json(songName + '/events');
 			#if sys
-			if (#if MODS_ALLOWED FileSystem.exists(Paths.modsJson(songName + '/events')) || #end FileSystem.exists(file))
+			if (#if MODS_ALLOWED FileSystem
 			#else
 			if (OpenFlAssets.exists(file))
 			#end
