a992db0452044876adf7ee83e4ca50b52f52dc17
SINIX_SKR <116418436+SINIXgo@users.noreply.github.com>
2023-03-17 22:31:40 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2971,7 +2971,7 @@ class PlayState extends MusicBeatState
 		var songName:String = Paths.formatToSongPath(SONG.song);
 		var file:String = Paths.json(songName + '/events');
 		#if sys
-		if (FileSystem.exists(Paths.modsJson(songName + '/events')) || FileSystem.exists(file)) {
+		if
 		#else
 		if (OpenFlAssets.exists(file)) {
 		#end
